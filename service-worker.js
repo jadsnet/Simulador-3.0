fa83e9cace9d89aa1a2bd3c077f5483a02faca73
Jadson Rodrigues <48611984+jadsnet@users.noreply.github.com>
2026-08-16 00:40:04 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="simulador-academy-v7-1-0-borderless-floating-headers";
+const CACHE="simulador-academy-v7-1-1-dark-headers-micro-controls";
 const ASSETS=["./","./index.html","./style.css","./app.js","./db.js","./cloud.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
